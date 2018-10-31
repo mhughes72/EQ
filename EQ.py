7a6538e99d76539be4e850b630b28df21b9bfb3d
Matt Hughes <matthughes72@gmail.com>
2018-10-31 09:54:56 -0400

    message

--- EQ.py
+++ EQ.py
@@ -20,8 +20,8 @@ def mapPOID02(accum):
 def mapCities02(accum):
     return Row(City=accum[0], Country=accum[1], ID_remove=accum[2], Province = accum[5])
 
-def myFunc(accum, n):
-    return accum + n
+def reduceByPOID(a, b):
+    return a + b
 
 def getMaxDistance(lines):
     allDist = []
@@ -39,18 +39,12 @@ def getMaxDistance(lines):
     elif (maxDist == lines[1][7]):
         return lines[0], lines[1][6], lines[1][7]
 
-
-def myFunc5(accum):
-    return (accum[0])
-
-conf = SparkConf().setMaster("local[4]").setAppName("RatingsHistogram")
+conf = SparkConf().setMaster("local[4]").setAppName("distanceWithPOIDHistogram")
 sc = SparkContext(conf = conf)
 spark = SparkSession.builder.config("spark.sql.warehouse.dir", "file:///C:/temp").appName("SparkSQL").getOrCreate()
 sqlContext = SQLContext(sc)
 
-
 POIData3  = sqlContext.read.option("header", "true").format("csv").load("POIList.csv")
-print(hasattr(POIData3, "registerTempTable"))
 POIData2 = POIData3.rdd.map(mapPOID01).toDF()
 POIData = POIData2.registerTempTable('POIData')
 
@@ -58,49 +52,21 @@ CityData3  = sqlContext.read.option("header", "true").format("csv").load("DataSa
 CityData2 = CityData3.rdd.map(mapCities01).toDF()
 CityData = CityData2.registerTempTable('CityData')
 
-
-
-airVelocityKMPH = [12,13,15,12,11,12,11]
-parVelocityKMPH = sc.parallelize(airVelocityKMPH)
-varianceValue = parVelocityKMPH.stdev()
-print(varianceValue)
-
-# def myFunc6(line):
-#     return line
-#
-#
-# myFilter = CityData.rdd.filter(myFunc6)
-# print(myFilter.take(5))
-
+#Using SQL to calculate the distance between Latitudes and Longitudes: ‘haversine’ formula
 sqlCalcDist = "SELECT c.ID, p.POIID, c.City, c.LatitudeCity, c.LongitudeCity, p.LatitudePOI, p.LongitudePOI, \
 (6371000 * acos(cos(radians(c.LatitudeCity)) * cos(radians(p.LatitudePOI)) * cos(radians(p.LongitudePOI) - radians(c.LongitudeCity)) + sin(radians(c.LatitudeCity)) * sin(radians(p.LatitudePOI)))) \
 AS distance \
 FROM POIData p CROSS JOIN CityData c ORDER BY distance"
 
-df3 = sqlContext.sql(sqlCalcDist).orderBy("ID")
-# df3.show(100)
-print(type(df3))
+dfWithDistance = sqlContext.sql(sqlCalcDist).orderBy("ID")
+distanceWithPOID = dfWithDistance.rdd.map(lambda l: ((l[0]), ((l[1]), (l[7]))))
 
-ratings = df3.rdd.map(lambda l: ((l[0]), ((l[1]), (l[7]))))
+onlyMaxDist = distanceWithPOID.reduceByKey(reduceByPOID).map(getMaxDistance)
+dfPOID = onlyMaxDist.map(mapPOID02).toDF()
+dfCities = CityData2.rdd.map(mapCities02).toDF()
 
-rbk = ratings.reduceByKey(myFunc).map(getMaxDistance)
-rbk2 = rbk.map(mapPOID02).toDF()
-rdd3 = CityData2.rdd.map(mapCities02).toDF()
+finalJoin = dfCities.join(dfPOID, dfCities.ID_remove == dfPOID.ID).drop('ID_remove')
 
-print(rdd3.show())
-
-finalJoin = rdd3.join(rbk2, rdd3.ID_remove == rbk2.ID).drop('ID_remove')
-
-print("HELLO - inner")
-print(finalJoin.show(5))
-
-# results = spark.sql(
-#   "SELECT * FROM people")
-# names = results.map(lambda p: p.name)
-# Apply functions to results of SQL queries.
-
-print ('MYMEAN 2')
-print(finalJoin.show(5))
 MyData = finalJoin.registerTempTable('MyData')
 
 sqlMeanStd = "SELECT stddev(c.distance) AS standev, mean(c.distance) AS mean FROM MyData c WHERE c.POIID = 'POI1' \
@@ -111,21 +77,9 @@ sqlMeanStd = "SELECT stddev(c.distance) AS standev, mean(c.distance) AS mean FRO
         UNION ALL \
         SELECT stddev(c.distance) AS standev, mean(c.distance) AS mean FROM MyData c WHERE c.POIID = 'POI4'"
 
-
 final1 = sqlContext.sql(sqlMeanStd)
-
-print("C")
-print(final1.show(5))
-
 finalJoin.groupBy("POIID").mean("distance").show(5)
 
-
-# finalJoin.groupBy("POIID").mean("distance").show(5)
-# st.stdev("distance").show(5)
-# np.std(finalJoin("distance"), axis=0).show(5)
-
-# f1.stddev("distance").show(5)
-# stddev(col)
 print(finalJoin.show())
 final1.toPandas().to_csv('EQFinalPOID.csv')
 finalJoin.toPandas().to_csv('EQFinalCities.csv')
